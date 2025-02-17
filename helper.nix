9625b6dd7f401a9e01e640bb574a1772b2432d82
flaaneurs <tinng.imp@gmail.com>
2025-02-17 11:36:23 +0000

    errors

--- helper.nix
+++ helper.nix
@@ -3,10 +3,10 @@
     let
       isNix = name: builtins.match ".*\\.nix$" name != null;
       isValid = file:
-        let module = import (dir + "/" + file);
+        let module = import (dir + file);
         in builtins.isAttrs module && builtins.hasAttr "config" module;
     in
-     map (file: import (dir + "/" + file))
+     map (file: import (dir + file))
        (
          builtins.filter (file: isNix file && isValid file)
            (builtins.attrNames (builtins.readDir dir))
