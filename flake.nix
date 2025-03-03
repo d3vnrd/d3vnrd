1967ea971f7f6610df178354eab259230bc8ff34
flaaneurs <tinng.imp@gmail.com>
2025-03-03 06:35:33 +0000

    testing

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Suckless NixUtils";
+  description = "FrameworkOS";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -9,5 +9,15 @@
     };
   };
 
-  outputs = inputs: import ./host inputs;
+  outputs = { self, nixpkgs, ... } @ inputs : let
+    inherit (nixpkgs) lib;
+    mylib = import ./lib { inherit lib; };
+    args = { inherit inputs lib mylib; };
+    for_all_systems = func: (lib.genAttrs (mylib.getDirNames ./host) func);
+  in {
+    nixosConfigurations = import ./host/x86_64-linux args;
+    formatter = for_all_systems (
+      system: nixpkgs.legacyPackages.${system}.alejndra
+    );
+  };
 }
--- host/default.nix
+++ host/default.nix
@@ -3,12 +3,9 @@
   mylib = import ../lib { inherit lib;};
   args = { inherit inputs lib mylib; };
 
-  nixosSystems = [
-    import ./x86_64-linux args
-  ];
-
   forAllSystems = func: (lib.genAttrs (mylib.getDirNames ./.) func);
 in {
+
   nixosConfigurations = import ./x86_64-linux args;
 
   formatter = forAllSystems (
