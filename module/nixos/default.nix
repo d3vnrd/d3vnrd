f1735c4ef6442b3aba1842475f8a7ab92bdebfe0
flaaneurs <tinng.imp@gmail.com>
2025-03-03 09:34:27 +0000

    testing

--- module/nixos/default.nix
+++ module/nixos/default.nix
@@ -3,7 +3,6 @@ let
   services = mylib.loadModules ./service;
   features = mylib.loadModules ./feature;
   extras = mylib.loadModules ./extra;
-  home = mylib.loadModules ./home;
 in
-{ imports = services ++ features ++ extras ++ home; }
+{ imports = services ++ features ++ extras; }
 
