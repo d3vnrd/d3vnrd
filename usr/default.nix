bf1543240cd6a7278042dbbde511b9f53f45e7fd
flaaneurs <tinng.imp@gmail.com>
2025-03-09 09:34:07 +0000

    new changes

--- usr/default.nix
+++ usr/default.nix
@@ -1,22 +1,19 @@
 { lib, mylib, nixpkgs, home-manager, ... } @ args: let
   genUsers = system: let
     users = mylib.filesIn ./${system};
-    setting = {
+  in lib.genAttrs users (
+    username: home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.${system}; 
       extraSpecialArgs = args;
       modules = [ 
-	./${system}/${username} 
+	./${system}/${username}.nix
 	../module/home
       ];
     };
-  in lib.genAttrs users (
-    username: home-manager.lib.homeManagerConfiguration setting
   );
+
 in {
-  #NOTE: user should be present on either standalone system or global accross other system
-  # --> Current approach only allows for specific system, for global purposes this require user.nix file to be present in all system folder, which is inefficient.
-  homeConfigurations = lib.mergeAttrsList [
-    genUsers
-  ];
+  homeConfigurations = lib.mergeAttrsList
+    ( map genUsers (mylib.dirsIn ./.) )
 }
 
