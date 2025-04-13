fb9a367d353b17bcb74d4e6126251dab80fb6344
tlmp59 <tlmp.imp@gmail.com>
2025-04-13 09:21:51 +0000

    fixing some problems

--- host/default.nix
+++ host/default.nix
@@ -1,6 +1,5 @@
-{ inputs, lib, util, ... }@specialArgs: let
+{ config, pkgs, inputs, lib, util, ... }@specialArgs: let
   inherit (inputs) nix-darwin home-manager;
-  inherit (util) mylib myvar;
 
   genHosts = system: let
     sysHosts = mylib.dirsIn ./${system};
@@ -15,13 +14,44 @@
       home = home-manager.nixosModules.home-manager;
     };
 
-  in with sysAttrs;
+  in with sysAttrs; with util;
     lib.genAttrs sysHosts (
       hostname: func {
         inherit system specialArgs;
         modules = [
 	  ./${system}/${hostname}/configuration.nix
-	  ( mylib.relativeToRoot "module/" )
+	  ( mylib.relativeToRoot "module/${type}" )
+	  {
+	    # ---General---
+	    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+	    networking.hostName = hostname;
+	    nixpkgs.config.allowUnfree = true;
+ 	    time.timeZone = "Asia/Vietnam";
+
+	    # ---Users setting---
+	    users.users.${myvar.user} = {
+	      isNormalUser = true;
+	      extraGroups = [ "wheel" ];
+	    }
+	    users.defaultUserShell = pkgs.zsh;
+
+	    # ---Reduce disk usage---
+	    nix.gc = {
+	      automatic = true;
+	      dates = "weekly";
+	      options = "--delete-older-than 1w";
+	    };
+	    nix.settings.auto-optimise-store = true;
+
+	    # ---Packages---
+	    environment.systemPackages = with pkgs; [
+	      neovim 
+	      git 
+	      gh
+	      wget
+	      curl
+	    ];
+	  }
         ];
       }
     );
