0a31f2ab0b2b6ad3ca6c590084dda98732107d8f
tlmp59 <tlmp.imp@gmail.com>
2025-04-13 09:39:26 +0000

    testing new approach

--- module/default.nix
+++ module/default.nix
@@ -1,1 +1,35 @@
-{ util, ... }: { imports = util.mylib.scanPath ./.; }
+{ config, pkgs, system, lib, util, ... }: with util;
+{
+  imports = [ if (lib.hasSuffix "darwin" system) then ./darwin else ./linux ];
+
+  # ---General---
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  networking.hostName = hostname;
+  nixpkgs.config.allowUnfree = true;
+  time.timeZone = "Asia/Vietnam";
+
+  # ---Users---
+  users.users.${myvar.user} = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
+  users.defaultUserShell = pkgs.zsh;
+
+  # ---Reduce disk usage---
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+  nix.settings.auto-optimise-store = true;
+
+  # ---Packages---
+  environment.systemPackages = with pkgs; [
+    neovim 
+    git 
+    gh
+    wget
+    curl
+  ];
+}
+
