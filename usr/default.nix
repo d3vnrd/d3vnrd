4b89f842954cc674ab078d03c741895c71040f92
tlmp59 <tlmp.imp@gmail.com>
2025-04-16 10:04:47 +0000

    add home-manager standalone installation (testing)

---
+++ usr/default.nix
@@ -0,0 +1,15 @@
+{ inputs, lib, mylib, systems, ...}: let
+  inherit (inputs) home-manager nixpkgs;
+
+  # WARN: same username but in different system directory can overide each others
+  genUsers = system: lib.genAttrs ( mylib.dirsIn ./${system} ) (
+    username: home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.${system};
+      modules = [
+        ./standalone.nix
+	../home $ --> Home modules
+      ];
+    }
+  )
+
+in { homeConfigurations = lib.mergeAttrsList ( map genUsers systems ) }
