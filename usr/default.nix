5fed20b4d8300e2d0228d2585f8be392f1d5bc88
tlmp59 <tlmp.imp@gmail.com>
2025-04-17 06:04:59 +0000

    Solve problem related to unique naming for external user

--- usr/default.nix
+++ usr/default.nix
@@ -1,15 +1,18 @@
-{ inputs, lib, mylib, systems, ...}: let
+{ inputs, lib, mylib, myvar, systems }: let
   inherit (inputs) home-manager nixpkgs;
 
   # WARN: same username but in different system directory can overide each others
-  genUsers = system: lib.genAttrs ( mylib.dirsIn ./${system} ) (
-    username: home-manager.lib.homeManagerConfiguration {
+  # --> Current approach is to make composite unique key for each user
+
+  genUsers = system: lib.listToAttrs ( map (name: {
+    name = "${name}-${myvar.suffix.${system}}"; 
+    value = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.${system};
-      modules = [
+      modules = [ 
         ./standalone.nix
 	../home # --> Home modules
       ];
-    }
-  );
+    };
+  }) (mylib.dirsIn ./${system}) );
 
 in { homeConfigurations = lib.mergeAttrsList ( map genUsers systems ); }
--- var/default.nix
+++ var/default.nix
@@ -1,3 +1,10 @@
 { lib }: {
   user = "tlmp59";
+
+  suffix = {
+    "x86_64-linux" = "x86l";
+    "x86_64-darwin" = "x86d";
+    "aarch64-linux" = "arcl";
+    "aarch64-darwin" = "arcd";
+  };
 }
