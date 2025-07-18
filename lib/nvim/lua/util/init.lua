68e67275fd6fbf516ea6d932859454522de5ee06
Tin Nguyen <tinng.imp@gmail.com>
2025-07-18 23:53:08 +0700

    Update init.lua

--- lib/nvim/lua/util/init.lua
+++ lib/nvim/lua/util/init.lua
@@ -23,7 +23,7 @@ M.vimrc = {
 M.setup = function(opts)
     M.vimrc = vim.tbl_deep_extend('force', M.vimrc, opts or {})
 
-    vim.api.nvim_create_user_command('CheckAll', function()
+    vim.api.nvim_create_user_command('checkcall', function()
         vim.cmd [[Lazy! load all]]
         vim.cmd [[checkhealth]]
     end, { desc = 'Load all plugins and run :checkhealth' })
