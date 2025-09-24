3a6b1138a8d32b0d7ede993479fd7e553a4cff4d
Devonroad <138866891+d3vnrd@users.noreply.github.com>
2025-09-24 09:04:29 +0000

    small update to enable ssh-agent for git authentication

--- module/home/default.nix
+++ module/home/default.nix
@@ -1,11 +1,14 @@
 {helper, ...}: {
   imports = helper.scanPath {path = ./.;};
 
+  services.ssh-agent.enable = true;
+
   programs.ssh = {
     enable = true;
     matchBlocks = {
       "vcs" = {
         host = "gitlab.com github.com";
+        addKeysToAgent = "confirm";
         identitiesOnly = true;
         identityFile = [
           "~/.ssh/vcs"
