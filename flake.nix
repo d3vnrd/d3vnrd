187ea091ceedeaad1456b9f22c1d3868972ac5f6
Devonroad <138866891+d3vnrd@users.noreply.github.com>
2025-09-24 08:50:13 +0000

    202524091549-update: sucessfully integrated sops-nix and secret repo into main flake

--- flake.nix
+++ flake.nix
@@ -64,6 +64,10 @@
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
     stylix.url = "github:danth/stylix";
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-    secrets.url = "git+ssh://git@github.com/d3vnrd/secrets.git?ref=main&shallow=1";
+
+    secrets = {
+      url = "git+ssh://git@github.com/d3vnrd/secrets.git?ref=main&shallow=1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }
--- module/home/default.nix
+++ module/home/default.nix
@@ -1,6 +1,19 @@
 {helper, ...}: {
   imports = helper.scanPath {path = ./.;};
 
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "vcs" = {
+        host = "gitlab.com github.com";
+        identitiesOnly = true;
+        identityFile = [
+          "~/.ssh/vcs"
+        ];
+      };
+    };
+  };
+
   home = {
     file = {};
     sessionVariables = {};
